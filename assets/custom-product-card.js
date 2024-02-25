05d95273179fd89a99975aa438ccf30ed3fa4ae2
mm-amanthakur <aman.k@marmeto.com>
2024-02-26 02:20:41 +0530

    customization

--- assets/custom-product-card.js
+++ assets/custom-product-card.js
@@ -5,39 +5,27 @@ class CustomProductCard extends HTMLElement {
         this.sectionId = this.dataset.sectionId;
         this.productId = this.dataset.productId;
         this.productUrl = this.dataset.productUrl;
-        console.log(this.productUrl)
-        this.variantData = JSON.parse(this.querySelector('.product-varients').textContent);
-        this.clicked = null
-        this.varients = Array.from(this.querySelectorAll('input[type="radio"]:checked'));
-        this.addEventListener('change', () => {
-            this.clicked = this.querySelector('input[type="radio"]:checked')
-            this.getVarient(this.clicked);
+        this.variantData = JSON.parse(this.querySelector('script').textContent);
+        console.log(this.variantData)
+        this.addEventListener('change', function () {
+            this.varients = Array.from(this.querySelectorAll('input[type="radio"]:checked'), input => input.value);
+            this.currentVarient = this.variantData.find(item => JSON.stringify(item.options) == JSON.stringify(this.varients))
+            this.getVarient(this.currentVarient.id)
         })
-
-
-
-        this.quickLook = this.querySelector(".badge-button")
-
-        this.quickLook.addEventListener('click', this.fetchPro.bind(this))
     }
 
-
-    fetchPro() {
-        console.log(this.productUrl)
-        fetch(this.productUrl)
-            .then(data => data.text())
-            .then((content) => {
-                const innerContent = new DOMParser().parseFromString(content, "text/html");
-                document.querySelector('body').innerHTML = innerContent.querySelector(`#shopify-section-template--22106840957230__slideshow_hero_banner_a49bzY`).innerHTML
-            })
-    }
+    // fetchPro() {
+    //     console.log(this.productUrl)
+    //     fetch(this.productUrl)
+    //         .then(data => data.text())
+    //         .then((content) => {
+    //             const innerContent = new DOMParser().parseFromString(content, "text/html");
+    //             document.querySelector('body').innerHTML = innerContent.querySelector(`#shopify-section-template--22106840957230__slideshow_hero_banner_a49bzY`).innerHTML
+    //         })
+    // }
 
     getVarient(myVar) {
-        const id = myVar.getAttribute("id");
-        this.currentVarient = this.variantData.find(varient => varient.id == id)
-        console.log(this.currentVarient.id)
-        const url = `/products/${this.productHandle}?variant=${this.currentVarient.id}&section_id=${this.sectionId}`;
-
+        const url = `/products/${this.productHandle}?variant=${myVar}&section_id=${this.sectionId}`;
         fetch(url)
             .then(data => data.text())
             .then((content) => {
@@ -45,8 +33,6 @@ class CustomProductCard extends HTMLElement {
                 this.innerHTML = innerContent.querySelector(`[data-product-handle='${this.productHandle}']`).innerHTML;
             })
     }
-
-
 }
 
 customElements.define('custom-product-card', CustomProductCard);
